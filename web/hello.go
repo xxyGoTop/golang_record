118d94f7cb659b4abb97988f3f8d8fc7e773a5a7
xxyGoTop <2724001387@qq.com>
2019-05-23 16:14:16 +0800

    Mod: add go mod

--- web/hello.go
+++ web/hello.go
@@ -10,54 +10,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type TankShot interface {
-	shot()
-}
-
-type TankDriver interface {
-	driver()
-}
-
-type TankShell interface {
-	shell()
-}
-
-type Tank struct {
-	shot   TankShot
-	driver TankDriver
-	shell  TankShell
-}
-
-func newTank(shot TankShot, driver TankDriver, shell TankShell) *Tank {
-	return &Tank{
-		shot:   shot,
-		driver: driver,
-		shell:  shell,
-	}
-}
-
-func (tank *Tank) doWork() {
-	tank.shot.shot()
-	tank.driver.driver()
-	tank.shell.shell()
-}
-
-type People1Shot struct {
-	TankShot
-}
-
-func (people *People1Shot) shot() {
-	fmt.Println("...shot")
-}
-
-type People2Shot struct {
-	TankShot
-}
-
-func (people *People2Shot) shot() {
-	fmt.Println("<<<shot")
-}
-
 type ContactDetails struct {
 	Email   string
 	Subject string
